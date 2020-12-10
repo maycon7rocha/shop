f3b85db7d30a05482674eff7c788e1bdf7cb0d7a
Maycon Rocha <maycon7rocha@gmail.com>
2020-12-10 13:09:08 -0300

    219

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:shop/providers/products.dart';
 import './views/products_overview_screen.dart';
-import './utils/app_routes.dart';
 import './views/product_detail_screen.dart';
+
+import './utils/app_routes.dart';
+
 import 'package:provider/provider.dart';
+
+import 'package:shop/providers/products.dart';
+import 'package:shop/providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => Products(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => Products(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => Cart(),
+        ),
+      ],
       child: MaterialApp(
         title: 'Minha Loja',
         theme: ThemeData(
--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,14 +1,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:shop/data/dummy_data.dart';
-import 'package:shop/models/product.dart';
+import 'package:shop/providers/product.dart';
 
 class Products with ChangeNotifier {
 
   List<Product> _items = DUMMY_PRODUCTS;
 
   List<Product> get items => [ ..._items ];
-  
+
   List<Product> get favoriteItems {
     return _items.where((prod) => prod.isFavorite).toList();
   }
