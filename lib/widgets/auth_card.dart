fd6dacc6da1bc1797706543f48740327253e8dc3
Maycon Rocha <maycon7rocha@gmail.com>
2020-12-30 11:36:06 -0300

    289

--- lib/widgets/auth_card.dart
+++ lib/widgets/auth_card.dart
@@ -10,6 +10,8 @@ class AuthCard extends StatefulWidget {
 }
 
 class _AuthCardState extends State<AuthCard> {
+  GlobalKey<FormState> _form = GlobalKey();
+  bool _isLoading = false;
   AuthMode _authMode = AuthMode.Login;
   final _passwordController = TextEditingController();
 
@@ -18,7 +20,38 @@ class _AuthCardState extends State<AuthCard> {
     'password': '',
   };
 
-  void _submit() {}
+  void _submit() {
+    // nao faz nada, caso nao esteja validado
+    if (!_form.currentState.validate()) {
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    if (_authMode == AuthMode.Login) {
+      // logn
+    } else {
+      // registrar
+    }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _swithAuthMode() {
+    if (_authMode == AuthMode.Login) {
+      setState(() {
+        _authMode = AuthMode.Signup;
+      });
+    } else {
+      setState(() {
+        _authMode = AuthMode.Login;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,68 +62,79 @@ class _AuthCardState extends State<AuthCard> {
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0),
       ),
-      child: Container(
-        height: 320,
-        width: deviceSize.width * 0.75,
-        padding: EdgeInsets.all(16.0),
-        child: Form(
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: 'E-mail'),
-                keyboardType: TextInputType.emailAddress,
-                validator: (value) {
-                  if (value.isEmpty || !value.contains('@')) {
-                    return "Informe um e-mail válido!";
-                  }
-                  return null;
-                },
-                onSaved: (value) => _authData['email'] = value,
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Senha'),
-                controller: _passwordController,
-                obscureText: true,
-                validator: (value) {
-                  if (value.isEmpty || value.length < 5) {
-                    return "Informe uma senha válida!";
-                  }
-                  return null;
-                },
-                onSaved: (value) => _authData['password'] = value,
-              ),
-              if (_authMode == AuthMode.Signup)
+      child: SingleChildScrollView(
+        child: Container(
+          height: _authMode == AuthMode.Login ? 330 : 380,
+          width: deviceSize.width * 0.75,
+          padding: EdgeInsets.all(16.0),
+          child: Form(
+            key: _form,
+            child: Column(
+              children: [
                 TextFormField(
-                  decoration: InputDecoration(labelText: 'Confirmar Senha'),
+                  decoration: InputDecoration(labelText: 'E-mail'),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value.isEmpty || !value.contains('@')) {
+                      return "Informe um e-mail válido!";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _authData['email'] = value,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Senha'),
                   controller: _passwordController,
                   obscureText: true,
-                  validator: _authMode == AuthMode.Signup
-                      ? (value) {
-                          if (value != _passwordController.text) {
-                            return "Senhas são diferentes!";
-                          }
-                          return null;
-                        }
-                      : null,
+                  validator: (value) {
+                    if (value.isEmpty || value.length < 5) {
+                      return "Informe uma senha válida!";
+                    }
+                    return null;
+                  },
                   onSaved: (value) => _authData['password'] = value,
                 ),
-              SizedBox(height: 20),
-              RaisedButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
+                if (_authMode == AuthMode.Signup)
+                  TextFormField(
+                    decoration: InputDecoration(labelText: 'Confirmar Senha'),
+                    obscureText: true,
+                    validator: _authMode == AuthMode.Signup
+                        ? (value) {
+                            if (value != _passwordController.text) {
+                              return "Senhas são diferentes!";
+                            }
+                            return null;
+                          }
+                        : null,
+                  ),
+                  Spacer(),
+                if (_isLoading)
+                  CircularProgressIndicator()
+                else
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    color: Theme.of(context).primaryColor,
+                    textColor: Theme.of(context).primaryTextTheme.button.color,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 30.0,
+                    ),
+                    child: Text(
+                      _authMode == AuthMode.Login ? 'ENTRAR' : 'REGISTRAR',
+                    ),
+                    onPressed: _submit,
+                  ),
+                FlatButton(
+                  onPressed: _swithAuthMode,
+                  child: Text(
+                    _authMode == AuthMode.Login ? 'REGISTRAR' : 'ENTRAR',
+                  ),
+                  textColor: Theme.of(context).primaryColor,
                 ),
-                color: Theme.of(context).primaryColor,
-                textColor: Theme.of(context).primaryTextTheme.button.color,
-                padding: EdgeInsets.symmetric(
-                  vertical: 8.0,
-                  horizontal: 30.0,
-                ),
-                child: Text(
-                  _authMode == AuthMode.Login ? 'ENTRAR' : 'REGISTRAR',
-                ),
-                onPressed: _submit,
-              )
-            ],
+              ],
+            ),
           ),
         ),
       ),
