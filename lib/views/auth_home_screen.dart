b6546bb99048c40104f167b2823516b3ce753729
Maycon Rocha <maycon7rocha@gmail.com>
2021-01-05 13:10:30 -0300

    301

--- lib/views/auth_home_screen.dart
+++ lib/views/auth_home_screen.dart
@@ -5,10 +5,21 @@ import 'package:shop/views/auth_screen.dart';
 import 'package:shop/views/products_overview_screen.dart';
 
 class AuthOrHomeScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     Auth auth = Provider.of(context);
-    return auth.isAuth ? ProductOverviewScreen() : AuthScreen();
+
+    return FutureBuilder(
+      future: auth.tryAutoLogin(),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (snapshot.error != null) {
+          return Center(child: Text('Ocorreu um erro!'));
+        } else {
+          return auth.isAuth ? ProductOverviewScreen() : AuthScreen();
+        }
+      },
+    );
   }
 }
